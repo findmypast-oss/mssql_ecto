aa57064836d60cdd72226c8a41d67d25231bb5bb
Steven Blowers <shdblowers@gmail.com>
2017-07-06 08:20:25 +0100

    minor version release 0.2.0 to allow for custom ports

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MssqlEcto.Mixfile do
 
   def project do
     [app: :mssql_ecto,
-     version: "0.1.4",
+     version: "0.2.0",
      description: "Ecto Adapter for Microsoft SQL Server. Using Mssqlex.",
      elixir: ">= 1.4.0",
      build_embedded: Mix.env == :prod,
