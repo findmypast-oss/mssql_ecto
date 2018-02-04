c8c4ffeda438345288aac51b6efc453bfeb6fd8d
Steven Blowers <shdblowers@gmail.com>
2018-02-04 23:39:39 +0000

    updating ecto to 2.2

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule MssqlEcto.Mixfile do
   defp deps do
     [
       {:mssqlex, "~> 1.0.0"},
-      {:ecto, "~> 2.1.0"},
+      {:ecto, "~> 2.2.0"},
       {:ex_doc, "~> 0.15", only: :dev, runtime: false},
       {:excoveralls, "~> 0.6", only: :test},
       {:inch_ex, "~> 0.5", only: :docs}
